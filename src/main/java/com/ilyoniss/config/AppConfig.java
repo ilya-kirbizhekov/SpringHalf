fab8c66446cf609bd56d92b5cf833aead37de13c
Ilya Kirbizhekov <ilyoniss@mail.ru>
2023-12-09 15:05:04 +1100

    s

---
+++ src/main/java/com/ilyoniss/config/AppConfig.java
@@ -0,0 +1,65 @@
+package com.ilyoniss.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.cfg.Environment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+import jakarta.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class AppConfig {
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean()
+    {
+
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(datasource());
+        sessionFactory.setPackagesToScan("com.ilyoniss.domain");
+        sessionFactory.setHibernateProperties(hibernateproperties());
+        return sessionFactory;
+
+    }
+
+    private Properties hibernateproperties() {
+
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.HBM2DDL_AUTO, "validate");
+        return properties;
+    }
+
+    @Bean
+    public DataSource datasource() {
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("com.p6spy.engine.spy.P6SpyDriver");
+        dataSource.setJdbcUrl("jdbc:p6spy:mysql://localhost:63306/todo");
+        dataSource.setUsername("root");
+        dataSource.setPassword("p@ssw0rd");
+        dataSource.setMaximumPoolSize(10);
+        return dataSource;
+
+    }
+
+    public TransactionManager transactionManager(EntityManagerFactory factory)
+    {
+
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(factory);
+        return transactionManager;
+
+    }
+
+
+}
---
+++ src/main/java/com/ilyoniss/domain/Status.java
@@ -0,0 +1,10 @@
+package com.ilyoniss.domain;
+
+public enum Status {
+
+    IN_PROGRESS,
+    DONE,
+    PAUSED
+
+
+}
